4ff5ede56db9b7c50baba1d5fda688517199dbaa
nicolasraffray <nraffray@gmail.com>
2020-02-04 17:19:29 +0000

    setup of index.html done

--- note-model.js
+++ note-model.js
@@ -14,7 +14,7 @@
     let a = el.join("</div></li><li><div>")
     return `<ul><li><div>${a}</div></li></ul>`
   }
-  
+
   Note.prototype.notes = function(){ return this.text }
   
   exports.newNote = Note
@@ -24,12 +24,3 @@
 
 new newNoteBook
 new newNote("some random text")
-
-
-
-// Code is in a file called note-model.js.
-// Code is wrapped in the module pattern.
-// Uses the constructor and prototype pattern to define a note model object that can be instantiated.
-// Takes the text of a note upon instantiation e.g. My favourite language is JavaScript.
-// Stores the text in a text property on the note.
-// Has a method that will return the note text.
