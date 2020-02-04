47d63a50ffc327c9ba2580bd703c195ac49c3eb1
nicolasraffray <nraffray@gmail.com>
2020-02-04 17:03:19 +0000

    challenge 3 completed

--- note-model.js
+++ note-model.js
@@ -3,19 +3,20 @@
   let Note = function(text) {
     this.text = text
   };
-
   let NoteBook = function() {
     this.storage = []
   };
 
-  NoteBook.prototype.saveNotes = function(notes){
-    this.storage.push(notes)
+  NoteBook.prototype.saveNotes = function(notes){ this.storage.push(notes) }
+  NoteBook.prototype.converttoHTML = function(){
+    console.log(this.storage)
+    let el = this.storage.map(function(val){return val.text})
+    let a = el.join("</div></li><li><div>")
+    return `<ul><li><div>${a}</div></li></ul>`
   }
-
-
-  Note.prototype.notes = function(){
-    return this.text
-  }
+  
+  Note.prototype.notes = function(){ return this.text }
+  
   exports.newNote = Note
   exports.newNoteBook = NoteBook
 
--- note-tests.js
+++ note-tests.js
@@ -1,33 +1,38 @@
 
 function testIsString(){
-let n = new newNote("Hello World")
-let t = n.text
-
-assert.isTrue(typeof t === 'string')
-assert.isTrue(n.text === "Hello World")
+  let n = new newNote("Hello World")
+  let t = n.text
+  assert.isTrue(typeof t === 'string')
+  assert.isTrue(n.text === "Hello World")
 };
-
 testIsString();
 
 function testHasArray(){
-
-let noteBook = new newNoteBook
-let notes = noteBook.storage
-let notesArray = []
-console.log(notes)
-console.log(notesArray)
-
-assert.isTrue(typeof notes === 'object')
+  let noteBook = new newNoteBook
+  let notes = noteBook.storage
+  let notesArray = []
+  assert.isTrue(typeof notes === 'object')
 };
 testHasArray();
 
-function storesNotesInArray(){
+function teststoresNotesInArray(){
   let noteBook = new newNoteBook
   let note = "some random note to test"
   noteBook.saveNotes(note)
   let n = noteBook.storage
-
-  console.log(n)
   assert.isTrue(n[0] === "some random note to test")
 };
-storesNotesInArray();
+teststoresNotesInArray();
+
+function testconverttoHTML(){
+  let noteBook = new newNoteBook
+  let note = new newNote("Some Random note")
+  let note2 = new newNote("Another Random note")
+  noteBook.saveNotes(note)
+  noteBook.saveNotes(note2)
+  console.log(noteBook.storage[0].text)
+  console.log(noteBook.converttoHTML())
+  n = "<ul><li><div>Some Random note</div></li><li><div>Another Random note</div></li></ul>"
+  assert.isTrue(noteBook.converttoHTML() === n )
+};
+testconverttoHTML()
