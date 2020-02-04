70563126e8173fe2de6af4c1267a2148e336240c
nicolasraffray <nraffray@gmail.com>
2020-02-04 17:05:19 +0000

    forgot save on test file

--- note-tests.js
+++ note-tests.js
@@ -30,8 +30,7 @@ function testconverttoHTML(){
   let note2 = new newNote("Another Random note")
   noteBook.saveNotes(note)
   noteBook.saveNotes(note2)
-  console.log(noteBook.storage[0].text)
-  console.log(noteBook.converttoHTML())
+  
   n = "<ul><li><div>Some Random note</div></li><li><div>Another Random note</div></li></ul>"
   assert.isTrue(noteBook.converttoHTML() === n )
 };
